6c23ba9ccd3b0484385139150904680485d757c1
Alena Nemtsova <jelenatbs@gmail.com>
2022-05-05 15:26:13 +0300

    add scss-styles

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './button.css'
+
+export default class Button extends React.Component {
+    render() {
+        let className = 'button';
+        if (this.props.success) {
+            className += '  button__success';
+        }
+        return (
+            <button className={className}>
+                Нажми меня
+            </button>
+        );
+    }
+}
---
+++ src/components/CardList.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+//Не забываем импортировать компонент
+import Card from './Card';
+
+
+class CardList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+            
+                <Card
+                    title="Зимние ботинки"
+                    price={99}
+                    description="Такие только у нас"
+                    imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8oUWAaHxvjq-XOZPRpgCcsV6kjXjVkBfu0A&usqp=CAU"
+                />
+                <Card
+                    title="Пляжные тапки"
+                    price={23}
+                    description="Надеюсь, пригодятся"
+                    imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq9sEN8ILRx19HsrWVD3VRe2AV7nud1BaMuw&usqp=CAU"
+                />
+                <Card
+                    title="Праздничные туфли"
+                    price={85}
+                    description="Теперь и в черном цвете"
+                    imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3um-PWxYFmKlYLvP3ARDBaXly71vnwh-IjQ&usqp=CAU"
+                />
+            
+            </React.Fragment>
+        );
+    }
+}
+
+export default CardList;

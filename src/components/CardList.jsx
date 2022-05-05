d51415ce3ec6a3d8ca1ca774191ba44b5f432d9a
Alena Nemtsova <jelenatbs@gmail.com>
2022-05-05 15:53:45 +0300

    add card selection

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-//Не забываем импортировать компонент
 import Card from './Card';
-
 
 class CardList extends React.Component {
     render() {
@@ -13,18 +11,21 @@ class CardList extends React.Component {
                     price={99}
                     description="Такие только у нас"
                     imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8oUWAaHxvjq-XOZPRpgCcsV6kjXjVkBfu0A&usqp=CAU"
+                    isSelected
                 />
                 <Card
                     title="Пляжные тапки"
                     price={23}
                     description="Надеюсь, пригодятся"
                     imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq9sEN8ILRx19HsrWVD3VRe2AV7nud1BaMuw&usqp=CAU"
+                    isSelected
                 />
                 <Card
                     title="Праздничные туфли"
                     price={85}
                     description="Теперь и в черном цвете"
                     imgLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3um-PWxYFmKlYLvP3ARDBaXly71vnwh-IjQ&usqp=CAU"
+                    isSelected
                 />
             
             </React.Fragment>
